3f61b1a369c7c34c13e3c05599ead6c8b6734783
Maluh2020 <maluhmyth@gmail.com>
2021-01-27 22:51:06 -0300

    Returning custom error page

--- Sales_web_MVC/Models/ViewModels/ErrorViewModel.cs
+++ Sales_web_MVC/Models/ViewModels/ErrorViewModel.cs
@@ -5,7 +5,7 @@ namespace Sales_web_MVC.Models.ViewModels
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
+        public  string Message { get; set; }
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
