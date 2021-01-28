3c1b261ef85c5f9612b9d81ff62eb2ca75222f3b
Maluh2020 <maluhmyth@gmail.com>
2021-01-28 00:16:56 -0300

    App locale, number and date formatting

--- Sales_web_MVC/Startup.cs
+++ Sales_web_MVC/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Sales_web_MVC.Data;
 using Sales_web_MVC.Services;
+using System.Globalization;
 
 namespace Sales_web_MVC
 {
@@ -48,6 +49,15 @@ namespace Sales_web_MVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
